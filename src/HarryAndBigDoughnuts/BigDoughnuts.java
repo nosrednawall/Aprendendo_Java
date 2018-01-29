a90f097b18844958ca0031aa88e1a9768a677729
nosrednawall <nosrednawall@gmail.com>
2018-01-29 09:26:13 -0200

    Adicionado comentários na classe BigDoughnuts.java

--- src/HarryAndBigDoughnuts/BigDoughnuts.java
+++ src/HarryAndBigDoughnuts/BigDoughnuts.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 public class BigDoughnuts {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //criação do objeto sc para ler o teclado
 		
-		int t,c,k,w;
+		int t,c,k,w; //declaracao das variáveis
 		
-		t=sc.nextInt();
+		t=sc.nextInt(); //leitura de variável
+		
 		while(t!=0) {
 			
 			c=sc.nextInt();
 			k=sc.nextInt();
 			w=sc.nextInt();
 			
-			if(c*w <= k) {
+			if( (c*w) <= k) {
 				System.out.println("yes");
 			}
 			else {
 				System.out.println("no");
-			}
-				
+			}	
 		}
 	}
-
 }
