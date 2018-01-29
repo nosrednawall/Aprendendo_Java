a6627146b8af7e2b04663afa9e5cbd1e571f2db4
nosrednawall <nosrednawall@gmail.com>
2018-01-29 09:23:25 -0200

    Terminado a classe BigDoughnuts.java

--- src/HarryAndBigDoughnuts/BigDoughnuts.java
+++ src/HarryAndBigDoughnuts/BigDoughnuts.java
@@ -1,6 +1,29 @@
 package HarryAndBigDoughnuts;
 
-public class BigDoughnuts {
+import java.util.Scanner;
 
+public class BigDoughnuts {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t,c,k,w;
+		
+		t=sc.nextInt();
+		while(t!=0) {
+			
+			c=sc.nextInt();
+			k=sc.nextInt();
+			w=sc.nextInt();
+			
+			if(c*w <= k) {
+				System.out.println("yes");
+			}
+			else {
+				System.out.println("no");
+			}
+				
+		}
+	}
 
 }
