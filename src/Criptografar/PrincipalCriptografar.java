f9825a06f1c2094a6f098f0298c04329c290792e
nosrednawall <nosrednawall@gmail.com>
2018-02-02 20:54:42 -0200

    Finalizado a classe PrincipalCriptografar

--- src/Criptografar/PrincipalCriptografar.java
+++ src/Criptografar/PrincipalCriptografar.java
@@ -9,7 +9,21 @@ public class PrincipalCriptografar {
 		System.out.println("Criptograr e descriptografar palavras");
 		
 		Menu();
-		int numero = Leitor();
+		int escolha = Leitor();
+		switch (escolha) {
+		
+		case 1:
+			Criptografar crypt = new Criptografar();
+			crypt.Criptografar();
+			break;
+			
+		case 2:
+			Descriptografar descrypt = new Descriptografar();
+			break;
+
+		default:
+			break;
+		}
 	}
 	
 	public static String Menu() {
@@ -32,7 +46,10 @@ public class PrincipalCriptografar {
 		
 		while(escolha > 3 || escolha < 1) {
 			System.out.print("\n Escolha errada! \n"
-					+ "Por favor escolha entre 1 a 3\n");
+					+ "Por favor escolha entre 1 a 3\n"
+					+ "1 para Encriptar \n"
+					+ "2 para Descriptar e \n"
+					+ "3 para sair \n");
 			escolha = leitor.nextInt();
 		}
 		return escolha;
